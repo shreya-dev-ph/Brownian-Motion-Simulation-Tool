54f15887d2857c41a0c4ef4486309bd42ffa893a
shreya-dev-ph <srayaseebiswas77@gmail.com>
2026-06-22 16:33:44 +0530

    Add files via upload

---
+++ src/brownian_motion_simulation/__init__.py
@@ -0,0 +1,19 @@
+from .core import (
+    motion_2d,
+    motion_3d,
+    collecting_data_2d,
+    collecting_data_3d,
+    plot_2d,
+    plot_3d,
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "motion_2d",
+    "motion_3d",
+    "collecting_data_2d",
+    "collecting_data_3d",
+    "plot_2d",
+    "plot_3d",
+]
